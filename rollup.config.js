4225a93cab0f8ffe7b7ca3fb53bff413f51805ac
ll <10617146+lateral-rotation@user.noreply.gitee.com3>
2023-06-05 21:14:33 +0800

    chore: 代码压缩

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: "./src/index.ts",
@@ -20,5 +21,6 @@ export default {
     commonjs(),
     typescript(),
     json(),
+    terser(),
   ],
 };
